caebb492036b59761161a96123048b4b112a097a
acarter881 <60497471+acarter881@users.noreply.github.com>
2020-10-11 13:05:40 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+#! python3
+# getOpenWeather.py - From the command line, prints out the current weather in a location
+
+import json, requests, sys
+
+APPID = 'YOUR APP ID AS A STRING' # Get your API key from https://openweathermap.org
+
+# Compute location from command line arguments
+if len(sys.argv) < 2:
+    print('Usage: getOpenWeather.py city_name, 2-letter_country_code OR 2-letter_state_abbreviation')
+    sys.exit()
+
+location = ' '.join(sys.argv[1:])
+
+# Download the JSON data from OpenWeatherMap.org's API
+url = 'https://api.openweathermap.org/data/2.5/weather?q={},&units=imperial&appid={}'.format(location, APPID)
+
+response = requests.get(url)
+response.raise_for_status()
+
+weatherData = json.loads(response.text)
+
+print('Current weather in {}'.format(location))
+print('Temperature: {}'.format(weatherData['main']['temp']) + ' degrees Fahrenheit')
+print('Description: {}'.format(weatherData['weather'][0]['description']).title())
